aa5231a408a08b729759f0a136eb76b342fd6f91
RownH <799698579@qq.com>
2020-02-10 13:54:06 +0800

    shared_ptr实现原理

--- smart_ptr/main.cpp
+++ smart_ptr/main.cpp
@@ -1,31 +1,71 @@
 #include <iostream>
 /*
-    最早auto_ptr实现
-    智能指针最基本的功能：对超出作用域的对象进行释放  从根本上消除资源泄漏的可能性
-    auto_ptr 是通过由 new 表达式获得的对象，并在 auto_ptr 自身被销毁时删除该对象的智能指针。它可用于为动态分配的对象提供异常安全、传递动态分配对象的所有权给函数和从函数返回动态分配的对象。
-    复制 auto_ptr ，会复制指针并转移所有权给目标： auto_ptr 的复制构造和复制赋值都会修改其右侧参数，而且“副本”不等于原值。因为这些不常见的复制语义，不可将 auto_ptr 置于标准容器中。此用途及其他使用更适合用 std::unique_ptr 。 (C++11 起)
+    unique_ptr的实现
 */
 
 using namespace std;
+//共享简化版  线程非安全版本
+class shared_count
+{
+public:
+    shared_count():m_count(1){}//初始化
+    void add_count(){
+       m_count++;
+    }//增加计数
+    long reduce_count(){
+        --m_count;
+        return m_count;
+    }//减少计数
+    long get_count()const{
+        return m_count;
+    }//获取计数
+private:
+    long m_count;
+};
+
+
 template<typename T>
 class smart_ptr{
 public:
     explicit smart_ptr(T * ptr=nullptr):m_ptr(ptr){
+        if(ptr){
 
+            m_ref_count=new shared_count();
+
+        }
+    }
+    template<typename U>
+    smart_ptr(const smart_ptr<U> &other){
+        m_ptr=other.m_ptr;
+        if(m_ptr){
+           other.m_ref_count->add_count();
+           m_ref_count=other.m_ref_count;
+        }
     }
     template<typename U>
     smart_ptr(smart_ptr<U> &&other){
-        m_ptr=other.relese(); //类型转换
+        m_ptr=other.m_ptr;
+        if(m_ptr){
+           m_ref_count=other.m_ref_count;
+           other.m_ptr=nullptr;
+        }
     }
-    smart_ptr(smart_ptr && ptr){
-        m_ptr=ptr.relese();
+    smart_ptr(const smart_ptr & ptr){
+        m_ptr=ptr.m_ptr;
+        if(m_ptr){
+           ptr.m_ref_count->add_count();
+           m_ref_count=ptr.m_ref_count;
+        }
     }
     smart_ptr &operator=( smart_ptr ptr){
         smart_ptr(ptr).swap(*this);   //构造临时对象  交换指针
         return  *this;
     }
     ~smart_ptr(){
-        delete  m_ptr;
+        if(!m_ref_count->reduce_count() && m_ptr){
+            delete  m_ref_count;
+            delete  m_ptr;
+        }
     }
     T * get(){
         return m_ptr;
@@ -46,16 +86,32 @@ public:
     }
     void swap(smart_ptr &rhs){
         std::swap(m_ptr,rhs.m_ptr);
+        std::swap(m_ref_count,rhs.m_ref_count);
+    }
+    long use_count(){
+        if(m_ptr){
+            return m_ref_count->get_count();
+        }
+        else{
+            return 0;
+        }
     }
 private:
     T * m_ptr;
+    shared_count * m_ref_count;
 };
 
 int main()
 {
-
     int *b=new int(1);
     smart_ptr<int>tempA(b);
-    cout<<*tempA;
+
+    cout<<tempA.use_count();
+    {
+       smart_ptr<int>tempB(tempA);
+       cout<<tempB.use_count();
+    }
+    smart_ptr<int>tempB(tempA);
+    cout<<tempB.use_count();
     return 0;
 }
